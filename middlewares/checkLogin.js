c19ad7afae771fd0fbbcfd05ec33c637eebd770f
Bbabam <sv741147vs@gmail.com>
2023-05-27 20:58:53 +0900

    credit 확인 AIP

--- middlewares/checkLogin.js
+++ middlewares/checkLogin.js
@@ -4,18 +4,23 @@ const { Users } = require("../models");
 module.exports = async(req, res, next) => {
     try {
         const { Authorization } = req.cookies;
-        const [authType, authToken] = Authorization.split(" ");
-        
-        //토큰이 있는지 확인 및 authTyep === Bearer인지 확인
+        console.log(Authorization)
+
+        //토큰이 있는지 확인
         if (!Authorization) {
             return res.status(403).json({ errorMessage: "로그인이 필요한 서비스입니다." });
         }
+
+        const [authType, authToken] = Authorization.split(" ");
+        console.log(Authorization, authType, authToken)
+        
+        //authTyep === Bearer인지 확인
         if (authType !== "Bearer" || !authToken) {
             return res.status(403).json({ errorMessage: "토큰 정보 오류" });
         }
 
         //
-        const { userId } = jwt.verify(authToken, "'chatGPT_key");
+        const { userId } = jwt.verify(authToken, "chatGPT_key");
         const user = await Users.findOne({ where: { userId } });
 
         res.locals.user = user;
@@ -23,6 +28,6 @@ module.exports = async(req, res, next) => {
 
     } catch (error) {
 
-        return res.status(403).json({ errorMessage: "로그인이 필요한 서비스입니다." });
+        return res.status(500).json({ errorMessage: "로그인이 필요한 서비스입니다." });
     }
 };
