f3e48b412968c36953fce6fb7f2e3932e55856d8
임은호 <>
2023-06-03 17:34:09 +0900

    fix credits migration file to include creditId

--- migrations/20230531143527-create-credits.js
+++ migrations/20230531143527-create-credits.js
@@ -3,7 +3,7 @@
 module.exports = {
     async up(queryInterface, Sequelize) {
         await queryInterface.createTable('Credits', {
-            id: {
+            creditId: {
                 allowNull: false,
                 autoIncrement: true,
                 primaryKey: true,
