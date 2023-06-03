eff1f3391c7f085f4f1ba4ee503c2359016cf87f
Juhyuck-Hong <juhyukhong@gmail.com>
2023-06-03 16:26:47 +0900

    Test CORS option

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use('/api', [indexRouter]);
 
 // CORS 미들웨어 함수 정의
 const allowCrossDomain = (req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
