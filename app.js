cdbe548ca410ebe8844f31fc9a3b46779556283b
Juhyuck-Hong <juhyukhong@gmail.com>
2023-06-03 15:57:08 +0900

    Test CORS option

--- app.js
+++ app.js
@@ -1,22 +1,24 @@
+const env = require('dotenv').config();
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const app = express();
+const { Users } = require('./models');
 
 const indexRouter = require('./routes/index.js');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use('/api', [indexRouter]);
 
 // CORS 설정
 const cors = require('cors');
 app.use(
     cors({
-        origin: ['https://team2-clone-fe.vercel.app', 'http://localhost:3000']
+        origin: ['https://team2-clone-fe.vercel.app', 'http://localhost:3000'],
+        credentials: true,
     })
 );
-
-app.use('/api', [indexRouter]);
 
 app.get('/', (req, res) => {
     res.status(200).send('chatGPT clone API');
@@ -25,3 +27,10 @@ app.get('/', (req, res) => {
 app.listen(3001, () => {
     console.log('3001 포트로 서버 연결');
 });
+
+// ❖ 24시간마다 모든유저 크레딧 10개로 갱신
+const intervalID = setInterval(myCallback, 86400000);
+
+async function myCallback() {
+    await Users.update({ credit: 10 }, { where: {} });
+}
--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,7 @@
 const express = require('express');
-const router = express.Router();
+const app = express();
 
-// CORS 설정
-const cors = require('cors');
-router.use(
-    cors({
-        origin: ['https://team2-clone-fe.vercel.app', 'http://localhost:3000']
-    })
-);
+const chats = require('./chats');
+const users = require('./users');
 
-// chats 및 users 라우터 가져오기
-const chatsRouter = require('./chats');
-const usersRouter = require('./users');
-
-// chats 및 users 라우터 등록
-router.use('/chats', chatsRouter);
-router.use('/users', usersRouter);
-
-module.exports = router;
+module.exports = [chats, users];
